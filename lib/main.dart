f14987b7a57b5a17de5285a334bce748efff612c
vedant dandge <vedantdandgepro@gmail.com>
2022-06-17 23:13:40 +0530

    Expense-Tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,27 +20,66 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
 
+  final List<Transaction> transactions = [
+  Transaction(
+    id: '1',
+  title: 'New Shoes',
+    amount: 69,
+    date: DateTime.now()
+  ),
+    Transaction(
+        id: '2',
+        title: 'socks',
+        amount: 10,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: '3',
+        title: 'slip-ons',
+        amount: 50,
+        date: DateTime.now()
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("flutter app"),
       ),
-      body: Column(
-        children:<Widget> [
-        Card(
-          color: Colors.amber,
-          child: Container(
-            child: Text("chart!"),
-            width: 100,
+      body: Container(
+        child: Column(
+          children:<Widget> [
+          Card(
+            color: Colors.amber,
+            child: Container(
+              child: Text("chart!"),
+              width: 100,
+            ),
+            elevation: 5,
           ),
-          elevation: 5,
-        ),
 
 
-        Card(child: Text("list of txt !"),
+          Column(children: transactions.map((tx) {
+            return Card(child:
+                Row (children: [
+                  Container(
+                    child: Text(tx.amount.toString(),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                    Text(tx.title),
+                    Text(
+                      tx.date.toString(),
+                    )
+                  ],)
+                ],)
+
+            );
+          }).toList(),),
+        ],
         ),
-      ],
       ),
     );
   }
